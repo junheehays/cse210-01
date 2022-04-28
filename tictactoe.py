3f608d91fed66f64df9cceec0e79f26d336456f3
Junhee Hays <junheemhays@gmail.com>
2022-04-28 08:40:46 -0500

    add the new file

---
+++ tictactoe.py
@@ -0,0 +1,12 @@
+# Author: Junhee Hays
+# Class: CSE210
+
+print("1|2|3")
+print("-+-+-")
+print("4|5|6")
+print("-+-+-")
+print("7|8|9")
+xone = input(print("x's turn to choose a square (1-9): "))
+print("I did it")
+print("This is great")
+print("Okay")
---
+++ tictactoereal.py
@@ -0,0 +1,3 @@
+# Author: Junhee Hays
+# Class: CSE210
+print("1 2 3")
