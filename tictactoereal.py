494755ffc7f672d57db692791958f045febeab29
Junhee Hays <junheemhays@gmail.com>
2022-04-30 22:01:53 -0500

    add the new file

--- tictactoereal.py
+++ tictactoereal.py
@@ -1,3 +1,11 @@
 # Author: Junhee Hays
 # Class: CSE210
-print("1 2 3")
+print("1|2|3")
+print("-+-+-")
+print("4|5|6")
+print("-+-+-")
+print("7|8|9")
+
+x = int(input(print("x's turn to choose a square (1-9): ")))
+
+while x
